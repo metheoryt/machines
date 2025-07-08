97d0b76425c17d7f775f7f0a88d8728726a49b8d
Maxim Romanyuk <metheoryt@gmail.com>
2025-07-08 17:19:54 +0500

    fix

--- hosts/g16/configuration.nix
+++ hosts/g16/configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
+  boot.loader.grub.devices = [ "nodev" ];
   boot.loader.grub.useOSProber = true;
 
   # Use latest kernel.
