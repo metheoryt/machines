594d33df8d96ac3afcd5ac0a26931641161a18e2
Maxim Romanyuk <metheoryt@gmail.com>
2026-07-26 17:55:03 +0500

    Revert "feat(starship): wire hermes-statusline into prompt via custom module"
    This reverts commit 417b31d83d95f814a181b000c4aba38536bd0714.

--- modules/home/me.nix
+++ modules/home/me.nix
@@ -272,7 +272,7 @@ in {
   programs.starship = {
     enable = true;
     settings = {
-      format = "$custom\n$all$character";
+      format = "$all$character";
       character = {
         success_symbol = "[➜](bold green)";
         error_symbol = "[➜](bold red)";
@@ -298,12 +298,7 @@ in {
         disabled = false;
         impure_msg = "[impure shell](bold red)";
         pure_msg = "[pure shell](bold green)";
-        format = "via [☃️ $state( \\($name\\))](bold blue) ";
-      };
-      custom.hermes = {
-        command = "bash ~/.hermes/hermes-statusline.sh";
-        when = "true";
-        shell = "bash";
+        format = "via [☃️ $state( \($name\))](bold blue) ";
       };
     };
   };
